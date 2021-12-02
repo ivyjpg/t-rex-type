b839d840d472c89cbcfa20e49f5cbf960a6ee524
ivyjpg <ivywna9@gmail.com>
2021-12-02 16:35:10 -0500

    level2

--- main.py
+++ main.py
@@ -29,9 +29,15 @@ letters = {"A": ["waz wsx as", "esx edc sd", "rdc rfv df", "tfv tgb fg", "ygb yh
            "Y": ["wsd edxz", "edf rfcx", "rfg tgvc", "tgh yhbv", "yhj ujnb", "ujk ikmn"],
            "Z": ["weszx", "erdxc", "rtfcv", "tygvb", "yuhbn", "uijnm"]}
 
+words = ["is", "was", "are", "be", "have", "had", "were", "can", "said", "use", "do", "will", "would", 
+         "make", "like", "has", "look", "write", "go", "see", "could", "been", "call", "am", "find",
+         "word", "time", "number", "way", "people", "water", "day",	"part",	"sound", "work", "place", 
+         "year", "back", "thing", "name", "sentence", "man", "line", "boy",	"farm", "end", "men", "land", 
+         "home", "hand"]
+
 score = 0
 user = input("enter your name: ")
-print("welcome, " + user + ".")
+print("welcome, " + user + ".\n")
 
 to_do = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", 
          "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -40,8 +46,8 @@ random.shuffle(to_do)
 print("LEVEL 1")
 for letter in to_do:
 
-    if score >= 7:
-        print("looks like you're getting the hang of this! \ntime to move on to the next level.")
+    if score >= 0:
+        print("looks like you're getting the hang of this! \ntime to move on to the next level.\n")
         break
 
     combos = letters[letter]
@@ -52,13 +58,45 @@ for letter in to_do:
     if (user_type == selected):
         print("correct!")
         score += 1
-        print("score: " + str(score))
+        print("score: " + str(score) + "\n")
     else:
         while (user_type != selected):
             print("sorry, try again")
             user_type = input(selected + "\n")
-        print("score: " + str(score))
-
+        print("score: " + str(score) + "\n")
 
 
 print("LEVEL 2")
+score = 0
+random.shuffle(words)
+
+print("rules: for each word, input one letter per line.\n")
+for word in words:
+
+    if score >= 5:
+        print("congrats! you've passed the level!")
+        break
+
+    num_lines = len(word)
+    print(num_lines)
+    print("type the following: " + word + "\n")
+    user_type = ""
+    for i in range(num_lines):
+        print(i)
+        user_type += input() + "\n"
+    
+    user_type = user_type.split("\n")
+    correct_flag = True
+
+    for j in range(len(user_type)-1):
+        cap_letter = word[j].upper()
+        if user_type[j] in letters[cap_letter]:
+            pass
+        else:
+            correct_flag = False
+    
+    if correct_flag:
+        score += 1
+        print("correct! score: " + str(score) + "\n")
+    else:
+        print("incorrect! score: " + str(score) + "\n")
